7b0c63be72d778c01a32b58c9350f2500047b328
nyananu <code.navigator01@gmail.com>
2020-09-11 17:28:53 -0700

    finished creating simple React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField';
 
 function App() {
+  const [myValue, setValue] = React.useState('<name>')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar color="primary" position="static">
+        <Toolbar>
+          <TypoGraphy variant="title"
+            color="inherit"
+          >
+            Hello User
+          </TypoGraphy>
+        </Toolbar>
+      </AppBar>
+      <br></br>
+      <TextField
+        id="standard-helperText"
+        label="Name"
+        defaultValue="Text"
+        helperText="Let's say hello!"
+        style={{
+          position: 'absolute', left: '50%', top: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <br></br>
+      <h1
+        style={{
+          position: 'absolute', left: '50%', top: '60%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+        Hello {myValue}!
+      </h1>
     </div>
   );
 }
